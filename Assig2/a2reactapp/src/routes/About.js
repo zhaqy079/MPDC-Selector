6225d4d7747f0afb502c1689749eeea7f718688c
Qianting <zhaqy079@mymail.unisa.edu.au>
2024-10-25 17:55:24 +1030

    Add Home page and About page structure

--- Assig2/a2reactapp/src/routes/About.js
+++ Assig2/a2reactapp/src/routes/About.js
@@ -1,1 +1,11 @@
-﻿
+﻿import React from 'react';
+
+function About() {
+    return (
+        <div className="bg-light py-1 mb-2">
+            <h1>About</h1>
+            <p>Contact info goes here...</p>
+        </div>
+    );
+}
+export default About;
--- Assig2/a2reactapp/src/routes/Home.js
+++ Assig2/a2reactapp/src/routes/Home.js
@@ -1,1 +1,11 @@
-﻿
+﻿import React from 'react';
+
+function Home() {
+    return (
+        <div className="bg-light py-1 mb-2">
+            <h1 className="row justify-content-center">Welcome to MPDC Site Selector</h1>
+            <p>Introduction content goes here...</p>
+        </div>
+    );
+}
+export default Home;
