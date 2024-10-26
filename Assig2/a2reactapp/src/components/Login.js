fa0d6d3b0259e3ce7a38c86c01577b2028bbffdd
Qianting <zhaqy079@mymail.unisa.edu.au>
2024-10-27 01:12:55 +1030

    Use useRef in login page to access theinput element without binding data

--- Assig2/a2reactapp/src/components/Login.js
+++ Assig2/a2reactapp/src/components/Login.js
@@ -1,10 +1,19 @@
-﻿import React, { useState } from 'react';
+﻿import React, { useState, useRef } from 'react';
 import SHA256 from 'crypto-js/sha256';
 import { Link } from "react-router-dom";
 
 function Login() {
+    const usernameRef = useRef();
+    const passwordRef = useRef();
+    const [error, setError] = useState('');
 
-
+    const loginForm = (e) => {
+        e.preventDefault();
+        // Get the value from Ref without binding
+        const username = usernameRef.current.value;
+        const password = passwordRef.current.value;
+        // Hash the password
+        const passwordHash = SHA256(password).toString();
 
 
     return (
@@ -34,11 +43,14 @@ function Login() {
 export default Login;
 
 //Reference list:
+//Main code:
 //How to encrypt password in React js before sending it to the API | Encrypt password using bcrypt js https://www.youtube.com/watch?v=ywlEPtiaHZg
 //crypto - js library: https://github.com/nodejs/node/blob/v23.1.0/lib/crypto.js
 //SHA256:
 //https://www.simplilearn.com/tutorials/cyber-security-tutorial/sha-256-algorithm
 //https://www.bilibili.com/video/BV1Ce411M7do/?spm_id_from=333.337.search-card.all.click&vd_source=9a8ebb3892f453e39e9ba1c60d275470
+//Use Fetch with async/await: https://dmitripavlutin.com/javascript-fetch-async-await/#2-fetching-json
 //Login form:
 //https://github.com/nauvalazhar/bootstrap-5-login-page/blob/master/index.html
 //https://getbootstrap.com/docs/5.3/forms/overview/#overview
+
