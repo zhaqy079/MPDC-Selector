1e350f4fb4e4708789a14a6a6c7a39b5c9f9914b
Qianting <zhaqy079@mymail.unisa.edu.au>
2024-10-26 13:32:06 +1030

    Adjust navbar font color and background constract

--- Assig2/a2reactapp/src/App.js
+++ Assig2/a2reactapp/src/App.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import SHA256 from 'crypto-js/sha256';
 import { Link, Outlet } from "react-router-dom";
 
+
 function App() {
   return (
     //<div className="App">
@@ -29,12 +30,24 @@ function App() {
     //</div>
       <div className="App container">
           {/*add nav bar*/}
-          <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: '#0F4D91' }}>
+          {/*logo reference: */}
+          {/*{nav bar reference:https://getbootstrap.com/docs/5.3/components/navbar/#nav}*/}
+          <nav className="navbar navbar-expand-lg" >
               <div className="container-fluid">
-                  <Link className="navbar-brand" to="/">MPDC Site Selector</Link>
+                  <Link className="navbar-brand" to="/Home">
+                      {/*add logo*/}
+                      <img
+                          src="./logo-black.png"
+                          alt="MPDC Icon"
+                          width="50"
+                          height="50" />
+                      <span> MPDC Site Selector</span>
+                  </Link>
+
                   <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                       <span className="navbar-toggler-icon"></span>
                   </button>
+
                   <div className="collapse navbar-collapse" id="navbarNav">
                       <div className="navbar-nav">
                           <Link className="nav-link active" to="/Home">Home</Link>
