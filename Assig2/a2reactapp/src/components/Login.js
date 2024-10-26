e86206c635069ce50039d078a5497ff3a68d2d2b
Qianting <zhaqy079@mymail.unisa.edu.au>
2024-10-27 00:26:05 +1030

    Add login form

--- Assig2/a2reactapp/src/components/Login.js
+++ Assig2/a2reactapp/src/components/Login.js
@@ -1,13 +1,29 @@
-﻿import React from 'react';
-import { useState, useEffect } from 'react';
+﻿import React, { useState } from 'react';
 import SHA256 from 'crypto-js/sha256';
 
 function Login() {
+
+
+
+
     return (
         <div className="loginpage" >
 
             <h2>Login</h2>
-
+            <form>
+                <div>
+                    <label className="form-label">User Name: </label>
+                    <input required type="text" className="form-control" placeholder="Username" />
+                </div>
+                <div>
+                    <label className="form-label">Password: </label>
+                    <input required type="password" className="form-control" placeholder="Password" />
+                </div>
+                <div>
+                    <p className="register"> Don't have an account? <br/> <span>Sign up at here</span></p>
+                </div>
+                <button type="submit" className="btn btn-info">Login</button>
+            </form>
         </div >
     );
 }
@@ -19,6 +35,6 @@ export default Login;
 //SHA256:
 //https://www.simplilearn.com/tutorials/cyber-security-tutorial/sha-256-algorithm
 //https://www.bilibili.com/video/BV1Ce411M7do/?spm_id_from=333.337.search-card.all.click&vd_source=9a8ebb3892f453e39e9ba1c60d275470
-//login form:
+//Login form:
 //https://github.com/nauvalazhar/bootstrap-5-login-page/blob/master/index.html
 //https://getbootstrap.com/docs/5.3/forms/overview/#overview
