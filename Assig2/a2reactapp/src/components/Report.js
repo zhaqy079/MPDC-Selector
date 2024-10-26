0a146bf223f32293694885e306f9778835437a0d
Qianting <zhaqy079@mymail.unisa.edu.au>
2024-10-26 23:52:21 +1030

    Add Report content

--- Assig2/a2reactapp/src/components/Report.js
+++ Assig2/a2reactapp/src/components/Report.js
@@ -1,1 +1,12 @@
-﻿
+﻿import React from 'react';
+
+function Report() {
+    return (
+        <div className = "reportpage" >
+
+            <h2>Report</h2>
+        
+        </div >
+    );
+}
+export default Report;
