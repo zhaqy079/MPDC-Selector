4e8a967464ac91c1be0a6d45e70d797f0fe0ff94
Qianting <zhaqy079@mymail.unisa.edu.au>
2024-10-28 01:51:50 +1030

    Update the dashboard header layout

--- Assig2/a2reactapp/src/components/Dashboard.js
+++ Assig2/a2reactapp/src/components/Dashboard.js
@@ -7,19 +7,34 @@ function Dashboard() {
     const username = localStorage.getItem('username');
     return (
         <div className="dashboard" >
-            <h2>Dashboard</h2>
-            {username && <p>Welcome, {username}!</p>}
-            <div className="filter-area">
-            <h5>Filter</h5>
+            <div className="dashboard-header">
+                <h2>Dashboard </h2>
+                {username && <p>Welcome, {username}!</p>}
+                
             </div>
-            <div className="search-results">
-                {/* Add search results content, will implement at part B's tasks' */}
-                <h4>Find Potential Mobile Phone Detection Camera Locations</h4>
-                <p>Select the filters below to search for potential locations for mobile phone detection cameras based on expiationDB.</p>
-                <Link to="/Report">
-                    <button type="button" className="btn btn-warning">Check Report</button>
-                </Link>
-                <button type="button" className="btn btn-outline-info">ReSearch</button>
+            <div className="dashboard-content">
+                <div className="filter-area">
+                    <h5>Filter</h5>
+                    <p>Select the filters below to search </p>
+                    <p>Select the filters below to search </p>
+                </div>
+                <div className="search-results">
+                    {/* Add search results content, will implement at part B's tasks' */}
+                    <h4>Find Potential Mobile Phone Detection Camera Locations</h4>
+                    <p>Select the filters below to search for potential locations for mobile phone detection cameras based on expiationDB.</p>
+                    <div className="row">
+                        <div className="col-4">
+                    <Link to="/Report">
+                        <button type="button" className="btn btn-warning">Check Report</button>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-4">
+                            <button type="button" className="btn btn-outline-info">ReSearch</button>
+                        </div>
+                    </div>
+                 </div>
             </div>
         </div>
     );
