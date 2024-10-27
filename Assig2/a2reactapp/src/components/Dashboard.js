42d9df297e677cfd3cb7729df7dfb71742d19071
Qianting <zhaqy079@mymail.unisa.edu.au>
2024-10-28 01:26:25 +1030

    Allocate the dashboard layout

--- Assig2/a2reactapp/src/components/Dashboard.js
+++ Assig2/a2reactapp/src/components/Dashboard.js
@@ -7,9 +7,20 @@ function Dashboard() {
     const username = localStorage.getItem('username');
     return (
         <div className="dashboard" >
-
             <h2>Dashboard</h2>
             {username && <p>Welcome, {username}!</p>}
+            <div className="filter-area">
+            <h5>Filter</h5>
+            </div>
+            <div className="search-results">
+                {/* Add search results content, will implement at part B's tasks' */}
+                <h4>Find Potential Mobile Phone Detection Camera Locations</h4>
+                <p>Select the filters below to search for potential locations for mobile phone detection cameras based on expiationDB.</p>
+                <Link to="/Report">
+                    <button type="button" className="btn btn-warning">Check Report</button>
+                </Link>
+                <button type="button" className="btn btn-outline-info">ReSearch</button>
+            </div>
         </div>
     );
 }
