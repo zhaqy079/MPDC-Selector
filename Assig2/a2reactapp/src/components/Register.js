8c8e560bdb9f8666083e1dc1d343766b19badb61
Qianting <zhaqy079@mymail.unisa.edu.au>
2024-10-28 00:59:01 +1030

    Add register form

--- Assig2/a2reactapp/src/components/Register.js
+++ Assig2/a2reactapp/src/components/Register.js
@@ -7,7 +7,28 @@ function Register() {
         <div className="registerpage" >
 
             <h2>Sign Up</h2>
-        </div>
+            <form onSubmit={registerForm}>
+                <div>
+                    <label className="form-label">User Name: </label>
+                    <input required type="text" className="form-control" placeholder="Username" ref={usernameRef} autoComplete="username" />
+                </div>
+                <div>
+                    <label className="form-label">Password: </label>
+                    <input required type="password" className="form-control" placeholder="Password" ref={passwordRef} autoComplete="password" />
+                </div>
+                <div>
+                    <label className="form-label">Confirm Password: </label>
+                    <input required type="password" className="form-control" placeholder="Confirm Password" ref={confirmPasswordRef} autoComplete="new-password" />
+                </div>
+                <div>
+                    <Link to="/Login">
+                        <p className="login-link"> Already have an account? <br /> <span>Login here</span></p>
+                    </Link>
+                </div>
+                {error && <p className="error">{error}</p>}
+                <button type="submit" className="btn btn-info">Register</button>
+            </form>
+        </div >
     );
 }
 export default Register;
