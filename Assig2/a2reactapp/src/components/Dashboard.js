74cfc678a350be01ee8c6b49485a15c9c6476262
Qianting <zhaqy079@mymail.unisa.edu.au>
2024-10-28 14:32:05 +1030

    Add Dashboard Filter layout and button

--- Assig2/a2reactapp/src/components/Dashboard.js
+++ Assig2/a2reactapp/src/components/Dashboard.js
@@ -17,7 +17,18 @@ function Dashboard() {
             <div className="dashboard-content">
                 <div className="filter-area">
                     <h5>Filter</h5>
+                    <div className="filter-options">
+                        <label className="form-label">Suburb</label>
                         <Suburb />
+                        <label className="form-label">Road</label>
+                        <Suburb />
+                        <label className="form-label">Date</label>
+                        <Suburb />
+                        <label className="form-label">Local Area</label>
+                        <Suburb />
+                        <button type="button" className="btn btn-info filter-button">Find Location</button>
+                    </div>
+
                 </div>
                 <div className="search-results">
                     {/* Add search results content, will implement at part B's tasks' */}
