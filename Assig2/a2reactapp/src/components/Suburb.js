e4eaaddc059412ce3cdb5bce4147dfaf2eaef4dd
Qianting <zhaqy079@mymail.unisa.edu.au>
2024-10-28 10:54:34 +1030

    Fetch the list of suburbs from the Endpoint API

--- Assig2/a2reactapp/src/components/Suburb.js
+++ Assig2/a2reactapp/src/components/Suburb.js
@@ -4,6 +4,15 @@ import { Link } from 'react-router-dom';
 function Suburb({ }) {
     // Set the state variable suburbs, and function setSuburbs to hold the list of suburbs
     const [suburbs, setSuburbs] = useState([]);
+    // Fetch the list of suburbs from the Endpoint API
+    useEffect(() => {
+        fetch(`http://localhost:5147/api/Get_ListCameraSuburbs`)
+            .then(response => response.json()) 
+            .then(data => setSuburbs(data))    
+            .catch(err => {
+                console.log(err); 
+            });
+    }, []); 
 
     return (
         <div className="btn-group suburb">
