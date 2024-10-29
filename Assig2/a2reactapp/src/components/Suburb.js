3c5a1dfac1ef8ec36d53f6fc20c1901df5ce7089
Qianting <zhaqy079@mymail.unisa.edu.au>
2024-10-29 21:25:58 +1030

    Set selected state to dynamically display user's selected suburb name

--- Assig2/a2reactapp/src/components/Suburb.js
+++ Assig2/a2reactapp/src/components/Suburb.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 function Suburb({ }) {
     // Set the state variable suburbs, and function setSuburbs to hold the list of suburbs
     const [suburbs, setSuburbs] = useState([]);
+    const [selected, setSelected] = useState([]);
+
     // Fetch the list of suburbs from the Endpoint API
     useEffect(() => {
         fetch(`http://localhost:5147/api/Get_ListCameraSuburbs`)
@@ -12,17 +14,22 @@ function Suburb({ }) {
             .catch(err => {
                 console.log(err); 
             });
-    }, []); 
-
+    }, []);
+    // Update Suburb selected button label to display user selected suburb
+    const userSelected = (suburb) => {
+        setSelected(suburb);
+    };
+    
     return (
         <div className="btn-group suburb">
-            <button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" >Suburb</button>
-            {/*Map over the suburbs array to create a list of dropdown items*/}
+            <button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" >{selected} </button>
+            {/*Map over the suburbs array to create a list of dropdown items
+            Ref: https://stackoverflow.com/questions/63233050/for-some-reason-after-mapping-onclick-event-on-li-wont-work*/}
             <ul className="dropdown-menu">
                 {suburbs.map((suburb) => (
                     <li key={suburb}>
                         <Link
-                            className="dropdown-item">
+                            className="dropdown-item" onClick={() => userSelected(suburb)}>
                             {/*to={`/suburb/${suburb}`} >*/}
                             {suburb} 
                         </Link>
