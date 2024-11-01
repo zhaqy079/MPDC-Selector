d14d1f55ddb54e31233e96fbddf5cd54d7dc519d
Qianting <zhaqy079@mymail.unisa.edu.au>
2024-11-01 22:04:44 +1030

    Add datalist tag to fetch the roadName

--- Assig2/a2reactapp/src/components/RoadName.js
+++ Assig2/a2reactapp/src/components/RoadName.js
@@ -1,14 +1,40 @@
 ﻿import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-function Road({ }) {
+function Road({ selectedSuburb}) {
+    // Set the state variable roads, and the function setRoad to hold the list of RoadName based on user selected suburb
+    const [roads, setRoads] = useState([]);
+
+    // Fetch the list of road name from the Get_ListCamerasInSuburb endpoint
+    // Refence: https://iqbalfn.github.io/bootstrap-autocomplete/
+    useEffect(() => {
+        if (selectedSuburb) {
+            fetch(`http://localhost:5147/api/Get_ListCamerasInSuburb?suburb=${selectedSuburb}&locationIdsOnly=false`)
+                .then(response => response.json())
+                .then(data =>
+                    setRoads(data.map(item => item.RoadName)))
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+        }, [selectedSuburb]);
+
     return (
         <div className="input-group has-validation">
             <div class="form-floating is-invalid">
                 <input
                 type="text"
                 className="form-control is-invalid"
-                placeholder="Road name" required/>
+                    placeholder="Road name"
+                    list="roadNameList"
+                    autoComplete="off"
+                    //onChange={(e) => setRoads(e.target.value)}
+                    required />
+                <datalist id="roadNameList">
+                    {roads.map((road, index) => (
+                        <option key={index} value={road} />
+                    ))}
+                </datalist>
             </div>
             <div class="invalid-feedback">
                  Please enter a Road Name.
