341853943b61b1efd7161a34d7818d3aaad4fa6d
Qianting <zhaqy079@mymail.unisa.edu.au>
2024-11-02 15:49:03 +1030

    Update Suburb selected button label to display user selected suburb and update the dashboard selected suburb

--- Assig2/a2reactapp/src/components/Suburb.js
+++ Assig2/a2reactapp/src/components/Suburb.js
@@ -1,10 +1,10 @@
 ﻿import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-function Suburb({ }) {
+function Suburb({ setSelectedSuburb }) {
     // Set the state variable suburbs, and function setSuburbs to hold the list of suburbs
     const [suburbs, setSuburbs] = useState([]);
-    const [selected, setSelected] = useState(["Suburb"]); // Update the select suburb dropdown item button default value
+    const [selected, setSelected] = useState(["Suburb"]); // Update the select suburb dropdown button display the default value "Suburb"
 
     // Fetch the list of suburbs from the Endpoint API
     useEffect(() => {
@@ -18,12 +18,13 @@ function Suburb({ }) {
 
     // Update Suburb selected button label to display user selected suburb
     const userSelected = (suburb) => {
-        setSelected(suburb);
+        setSelected(suburb); // Update the button, display the user's selected suburb 
+        setSelectedSuburb(suburb); // Update the selected suburb in the Dashboard component state
     };
-    
+
     return (
-        <div className="btn-group suburb">
-            <button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" >{selected} </button>
+        <div className="btn-group suburb has-validation">
+            <button type="button" className="btn btn-light dropdown-toggle is-invalid" data-bs-toggle="dropdown" >{selected} </button>
             {/*Map over the suburbs array to create a list of dropdown items
             Ref: https://stackoverflow.com/questions/63233050/for-some-reason-after-mapping-onclick-event-on-li-wont-work*/}
             <ul className="dropdown-menu">
@@ -34,6 +35,9 @@ function Suburb({ }) {
                             {/*to={`/suburb/${suburb}`} >*/}
                             {suburb} 
                         </Link>
+                        <div class="invalid-feedback">
+                            Please choose a suburb.
+                        </div>
                     </li>
                 ) )}
             </ul>
