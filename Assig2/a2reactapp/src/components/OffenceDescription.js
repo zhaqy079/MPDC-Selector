59aa0e5e0f50d4ece3461e612886bc35e417ec90
Qianting <zhaqy079@mymail.unisa.edu.au>
2024-11-02 22:57:46 +1030

    Set state varible and fetch the offence description from endpoint

--- Assig2/a2reactapp/src/components/OffenceDescription.js
+++ Assig2/a2reactapp/src/components/OffenceDescription.js
@@ -1,17 +1,31 @@
-﻿import { useState, useEffect } from 'react';
+﻿import { useState, useEffect, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 
-function Offence({ }) {
+function Offence({ offenceDesciption }) {
+    // Set the state variable for hold the list of offence description
+    const [offences, setOffences] = useState([]);
 
+    // Fetch the list of offences descriptions from Get_SearchOffencesByDescription
+    useEffect(() => {
+        fetch(`http://localhost:5147/api/Get_SearchOffencesByDescription?searchTerm=${offenceDesciption}&offenceCodesOnly=false`)
+            .then(response => response.json())
+            .then(data => setOffences(data))
+            .catch(err => {
+                console.log(err);
+            });
+
+    }, [offenceDesciption]);
 
 
     return (
         <div className="form-group">
             <select className="form-select">
                 <option selected>Description</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+                {offences.map((offence, index) => (
+                    <option key={index} value={offence.description} />
+                ))}
+
+
             </select>
         </div>
 
