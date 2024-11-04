6557adcfaa1c7a6ca05d0f64e8b74afdd06b7e76
Qianting <zhaqy079@mymail.unisa.edu.au>
2024-11-04 14:39:22 +1030

    Update Offence description, user can selection all the offence reason

--- Assig2/a2reactapp/src/components/OffenceDescription.js
+++ Assig2/a2reactapp/src/components/OffenceDescription.js
@@ -1,31 +1,36 @@
 ﻿import { useState, useEffect, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 
-function Offence({ offenceDesciption }) {
+function Offence() {
     // Set the state variable for hold the list of offence description
     const [offences, setOffences] = useState([]);
+    // Set the state to track the selected offence
+    const [selectedOffence, setSelectedOffence] = useState("");
 
     // Fetch the list of offences descriptions from Get_SearchOffencesByDescription
     useEffect(() => {
-        fetch(`http://localhost:5147/api/Get_SearchOffencesByDescription?searchTerm=${offenceDesciption}&offenceCodesOnly=false`)
+        fetch(`http://localhost:5147/api/Get_SearchOffencesByDescription?offenceCodesOnly=false`)
             .then(response => response.json())
             .then(data => setOffences(data))
             .catch(err => {
                 console.log(err);
             });
 
-    }, [offenceDesciption]);
+    }, []);
 
 
     return (
         <div className="form-group">
-            <select className="form-select">
-                <option selected>Description</option>
+            <select
+                className="form-select"
+                value={selectedOffence}
+                onChange={(e) => setSelectedOffence(e.target.value)}>
+                <option value="">Description</option>
                 {offences.map((offence, index) => (
-                    <option key={index} value={offence.description} />
+                    <option key={index} value={offence.description}>
+                        {offence.description}
+                    </option>
                 ))}
-
-
             </select>
         </div>
 
