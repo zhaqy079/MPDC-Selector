a967837f43dd75850eb48364c287c2411bde4637
Qianting <zhaqy079@mymail.unisa.edu.au>
2024-11-13 10:45:30 +1030

    Add RecommendedLocationViewModel

---
+++ Assig2/ViewModel/RecommendedLocationViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Assig2.ViewModel
+{
+	public class RecommendedLocationViewModel
+	{
+        public string Suburb { get; set; }
+        public string RoadName { get; set; }
+        public string LocalServiceArea { get; set; }
+        public int OffenceCount { get; set; }
+    }
+}
+
